03d6298160d2838af625c19a225e398ff187e5a9
askrynnikov <5101233@gmail.com>
2016-11-17 21:15:17 +0300

    Lesson 04 Slice file train

--- lesson_03/railway_management.rb
+++ lesson_03/railway_management.rb
@@ -1,6 +1,8 @@
 require_relative 'lib/station'
 require_relative 'lib/route'
 require_relative 'lib/train'
+require_relative 'lib/passenger_train'
+require_relative 'lib/cargo_train'
 
 def print_numbers_trains(station)
   puts "На станции #{name} находятся поезда:"
@@ -12,27 +14,27 @@ def print_amount_trains_by_type(station)
   puts station.amount_trains_by_type.to_a
 end
 
-# mosсow = Station.new('Москва')
-# piter = Station.new('Санкт-Петербург')
-# route_mosсow_piter = Route.new(mosсow, piter)
-# bologoe = Station.new('Бологое')
-# chudovo = Station.new('Чудово')
-# tver = Station.new('Тверь')
-# route_mosсow_piter.add_intermediate_station(bologoe)
-# route_mosсow_piter.add_intermediate_station(chudovo)
-# route_mosсow_piter.add_intermediate_station(tver, bologoe)
-#
-# train_209 = Train.new('209', :passenger, 12)
-# train_209.load_route(route_mosсow_piter)
-#
-# train_209.go_to_next_station
-# print tver.numbers_trains
-# train_209.go_to_next_station
-# print bologoe.numbers_trains
-# bologoe.send_train(train_209)
-# print chudovo.numbers_trains
-#
-# print route_mosсow_piter[0].name
+mosсow = Station.new('Москва')
+piter = Station.new('Санкт-Петербург')
+route_mosсow_piter = Route.new(mosсow, piter)
+bologoe = Station.new('Бологое')
+chudovo = Station.new('Чудово')
+tver = Station.new('Тверь')
+route_mosсow_piter.add_intermediate_station(bologoe)
+route_mosсow_piter.add_intermediate_station(chudovo)
+route_mosсow_piter.add_intermediate_station(tver, bologoe)
+
+train_209 = Train.new('209', :passenger, 12)
+train_209.load_route(route_mosсow_piter)
+
+train_209.go_to_next_station
+print tver.numbers_trains
+train_209.go_to_next_station
+print bologoe.numbers_trains
+bologoe.send_train(train_209)
+print chudovo.numbers_trains
+
+print route_mosсow_piter[0].name
 
 train_210 = PassengerTrain.new('210', 2)
 cargo_211 = CargoTrain.new('211', 5)
