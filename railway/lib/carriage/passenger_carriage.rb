166d389e458abe14b415b704bc3ceb4c08d0d536
askrynnikov <5101233@gmail.com>
2016-11-23 22:30:09 +0300

    Lesson 07 add seats in PassengerCarriage

--- railway/lib/carriage/passenger_carriage.rb
+++ railway/lib/carriage/passenger_carriage.rb
@@ -1,7 +1,19 @@
 require_relative '../carriage'
 
 class PassengerCarriage < Carriage
-  def initialize
+  attr_reader :number_seats, :number_occupied_seats
+
+  def initialize(number_seats)
     super(:passenger)
+    @number_seats = number_seats
+    @number_occupied_seats = 0
+  end
+
+  def take_seat
+    number_occupied_seats += 1 if number_occupied_seats < number_seats
+  end
+
+  def number_empty_seats
+    number_seats - number_occupied_seats
   end
 end
